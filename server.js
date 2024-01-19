d2f2ade1f1738d3bf682752a9452dea855f0acda
FacundoRinon <facuzionff@gmail.com>
2024-01-19 18:11:48 -0300

    path integrated

--- server.js
+++ server.js
@@ -3,17 +3,19 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const methodOverride = require("method-override");
-const APP_PORT = process.env.APP_PORT || 3000;
+const path = require("path");
 
 const sessions = require("./sessions");
 const passport = require("./passport");
 const routes = require("./routes");
 
+const APP_PORT = process.env.APP_PORT || 3000;
 const app = express();
 
 app.use(cors());
+
 app.use(methodOverride("_method"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
